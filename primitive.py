6ddb17dee1f4b349edf1ac0a72303cd1fef888cc
Kurt Barry <kurt.m.barry@gmail.com>
2023-07-27 14:56:21 -0400

    spot price and swap math

--- primitive.py
+++ primitive.py
@@ -29,7 +29,44 @@ def invariant_scipy(x, y, strike, sigma, tau):
     yTerm = cdfinv_scipy(np.float64(y) / strike)
     xTerm = cdfinv_scipy(np.float64(1) - x)
     sigmaSqrtTau = sigma * np.sqrt(tau / np.float64(SECONDS_PER_YEAR))
-    print(yTerm)
-    print(xTerm)
-    print(sigmaSqrtTau)
+#    print(yTerm)
+#    print(xTerm)
+#    print(sigmaSqrtTau)
     return yTerm - xTerm + sigmaSqrtTau
+
+# "implied" y value given other params (subtracted from "real" y value in original invariant formula)
+def implied_y(x, strike, sigma, tau):
+    tau = tau / mp.mpf(SECONDS_PER_YEAR)
+    return strike * mp.ncdf(cdfinv(mp.mpf(1) - x) - sigma * mp.sqrt(tau))
+
+def invariant_original(x, y, strike, sigma, tau):
+    return y - implied_y(x, strike, sigma, tau)
+
+def spot_price_volatile_asset(x, y, strike, sigma, tau):
+    tau = tau / mp.mpf(SECONDS_PER_YEAR)
+    price = strike
+    price *= mp.exp(cdfinv(mp.mpf(1) - x) * sigma * tau)
+    price *= mp.exp(mp.mpf(-0.5) * mp.power(sigma, 2) * tau)
+    return price
+
+# will return a negative number as dx postive => dy negative
+def get_dy(x, strike, sigma, tau, dx):
+    assert(dx >= 0)
+    return implied_y(x + dx, strike, sigma, tau) - implied_y(x, strike, sigma, tau)
+
+def get_y(x, y, strike, sigma, tau, dx):
+    assert(dx >= 0)
+    return y + get_dy(x, y, strike, sigma, tau, dx)
+
+def get_x(x, y, strike, sigma, tau, dy):
+    assert(dy >= 0)
+
+    # call other functions PRIOR to scaling tau
+    k = invariant_original(x, y, strike, sigma, tau)
+    tau = tau / mp.mpf(SECONDS_PER_YEAR)
+    return 1 - mp.ncdf(cdfinv((mp.mpf(y) + dy - k) / strike) + sigma * mp.sqrt(tau))
+
+# will return a negative number as dy postive => dx negative
+def get_dx(x, y, strike, sigma, tau, dy):
+    assert(dy >= 0)
+    return get_x(x, y, strike, sigma, tau, dy) - x
